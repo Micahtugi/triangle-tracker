c272c125d176847ed6971a51cb3cc5b07806048a
Micah <micahkimathi@gmail.com>
2019-03-29 01:15:50 +0300

    addition of the heading to the html file

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ function triangleTracker(){
   var w = parseInt(document.getElementById("w").value);
   var h = parseInt(document.getElementById("h").value);
   if (l===w && w===h && l===h){
-    alert("This is an Equilateral triangle");
+    alert(" Equilateral triangle");
   }
   else if (l===w ||w===h ||  l===h) {
-    alert("These in an Isosceles triangle");
+    alert("Isosceles triangle");
 
   }
   else if ((l+w)<=h || (l+h)<=w || (w+h)<=l){
-    alert("This is NOT a triangle");
+    alert(" NOT a triangle");
   }
   else if (l!==w && w!==h) {
-    alert("This is a Scalene triangle");
+    alert(" Scalene triangle");
   }
 }
