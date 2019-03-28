ff07c46f0ffa1d385f5846fa2d22a47ec93129cf
Micah <micahkimathi@gmail.com>
2019-03-29 00:26:03 +0300

    added equilateral triangle to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ function triangleTracker(){
   var l = parseFloat(document.getElementById("l").value);
   var w = parseFloat(document.getElementById("w").value);
   var h = parseFloat(document.getElementById("h").value);
+  if (l===w && w===h && l===h){
+    alert("This is an Equilateral triangle");
+  }
+  
 }
