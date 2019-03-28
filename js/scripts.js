a2f4db39cdfabc77b15125c068b7c3661b8cfaed
Micah <micahkimathi@gmail.com>
2019-03-29 00:22:53 +0300

    added variations to the javascript file

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function triangleTracker(){
+  var l = parseFloat(document.getElementById("l").value);
+  var w = parseFloat(document.getElementById("w").value);
+  var h = parseFloat(document.getElementById("h").value);
+}
