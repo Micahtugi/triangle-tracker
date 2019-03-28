6ecc49a4fbc7cade617589d2436efab2ed4bc3bc
Micah <micahkimathi@gmail.com>
2019-03-29 00:36:27 +0300

    added isosceles triangle to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 function triangleTracker(){
-  var l = parseFloat(document.getElementById("l").value);
-  var w = parseFloat(document.getElementById("w").value);
-  var h = parseFloat(document.getElementById("h").value);
+  var l = parseInt(document.getElementById("l").value);
+  var w = parseInt(document.getElementById("w").value);
+  var h = parseInt(document.getElementById("h").value);
   if (l===w && w===h && l===h){
     alert("This is an Equilateral triangle");
   }
-  
+  else if (l===w ||w===h ||  l===h) {
+    alert("These in an Isosceles triangle");
+
+  }
 }
